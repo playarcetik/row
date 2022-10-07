f759d05c27e8af80b9682159934e5e6fe19c9296
Keyur <keyur@Keyurs-MacBook-Air.local>
2022-10-07 07:54:08 +0530

    first commit

---
+++ lib/ui/row.dart
@@ -0,0 +1,297 @@
+import 'package:flutter/material.dart';
+
+class HomePage2 extends StatefulWidget {
+  const HomePage2({super.key});
+
+  @override
+  State<HomePage2> createState() => _HomePage2State();
+}
+
+class _HomePage2State extends State<HomePage2> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.blue,
+            //     ),
+            //     // SizedBox(
+            //     //   width: 50,
+            //     // ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.blueAccent,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.blueGrey,
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amber,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amberAccent,
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   // crossAxisAlignment: CrossAxisAlignment.end,
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.black,
+            //     ),
+            //   ],
+            // ),
+    
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.end,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.cyan,
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.cyan,
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.start,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.cyan,
+            //     ),
+            //   ],
+            // ),
+    
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amber,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amber,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amber,
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amber,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amber,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amber,
+            //     ),
+            //   ],
+            // ),
+    
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.blue,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.blueAccent,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.blueGrey,
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amber,
+            //     ),
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.amberAccent,
+            //     ),
+            //   ],
+            // ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     Container(
+            //       height: 100,
+            //       width: 100,
+            //       color: Colors.black,
+            //     ),
+            //   ],
+            // ),
+    
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                  color: Colors.indigoAccent,
+                    image: DecorationImage(image: AssetImage("assets/images/2.png"),fit: BoxFit.cover)
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration:const BoxDecoration(
+                  color: Colors.indigoAccent,
+                     image: DecorationImage(image: AssetImage("assets/images/2.png"),fit:BoxFit.cover)
+                  ),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.indigoAccent,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
